52b6ae02b90e7b0551f3767fc42287ac4fdd9284
Anderson Nascimento <anderson.aje57@gmail.com>
2026-04-24 11:27:46 -0300

    fix: tsconfig moderno NodeNext

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,3 +8,14 @@ export default {
     path: "prisma/migrations",
   },
 };
+
+/*import "dotenv/config";
+
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations"
+  }
+});*/
